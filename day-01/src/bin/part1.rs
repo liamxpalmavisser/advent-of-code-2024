103a49c91284bc73de54e93877d0e6d9e7739e02
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-01 11:16:26 +0100

    initial part1 working solution

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -4,8 +4,47 @@ fn main() {
     dbg!(output);
 }
 
+fn create_grid(lines: &str) -> Vec<Vec<char>> {
+    lines.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn get_columns(grid: Vec<Vec<char>>) -> (Vec<u32>, Vec<u32>) {
+    let second_column: Vec<u32> = grid
+        .iter()
+        .filter_map(|row| row.get(8..13))
+        .map(|slice| {slice.iter().collect::<String>().parse::<u32>().unwrap()})
+        .collect();
+    let first_column: Vec<u32> = grid
+        .iter()
+        .filter_map(|row| row.get(0..5))
+        .map(|slice| {slice.iter().collect::<String>().parse::<u32>().unwrap()})
+        .collect();
+
+    (first_column, second_column)
+}
+
+fn sort_elements(mut column: Vec<u32>) -> Vec<u32> {
+    column.sort_unstable_by(|a, b| b.cmp(a));
+    column
+}
+
+fn get_differences(column_1: Vec<u32>, column_2: Vec<u32>) -> u32 {
+    let mut distance_vec: Vec<u32> = Vec::new();
+    for (a, b) in column_1.iter().zip(column_2.iter()) {
+        distance_vec.push(a.abs_diff(*b))
+    }
+    let total_distance: u32 = distance_vec.iter().sum();
+    total_distance
+}
+
 fn part1(_input: &str) -> u32 {
-    32
+    let grid = create_grid(_input);
+    let (first_column, second_column) = get_columns(grid);
+    let first_sorted_column = sort_elements(first_column);
+    let second_sorted_column = sort_elements(second_column);
+
+    let total_differences = get_differences(first_sorted_column, second_sorted_column);
+    total_differences
 }
 
 #[cfg(test)]
@@ -14,8 +53,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let input = include_str!("./example1.txt");
+        let input = include_str!("./example2.txt");
         let result = part1(input);
-        assert_eq!(result, 142 as u32);
+        assert_eq!(result, 11 as u32);
     }
 }
