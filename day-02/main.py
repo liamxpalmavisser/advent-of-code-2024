0cac571c72f6f92364e130cabc0f7b43bec4f7f2
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-02 17:44:09 +0100

    Reformat

--- day-02/main.py
+++ day-02/main.py
@@ -19,17 +19,18 @@ def row_is_safe(row: list[int]) -> bool:
         diff = j - i
         if (diff == 0) | (abs(diff) > 3):
             return False
-        
+
         new_trend = Trend(1 if diff > 0 else -1)
         if trend and trend != new_trend:
             return False
-        
+
         trend = new_trend
     return True
 
 
 def can_be_safe_by_removal(row: list[int]) -> bool:
     return any(row_is_safe(row[:i] + row[i + 1 :]) for i in range(len(row)))
+
 
 def main():
     rows = parse("input.txt")
@@ -39,6 +40,7 @@ def main():
 
     n_safe_with_removal = sum(can_be_safe_by_removal(row) for row in rows)
     print(f"Part 2 solution {n_safe_with_removal}")
+
 
 if __name__ == "__main__":
     main()
