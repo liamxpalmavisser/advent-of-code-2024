4fbe149b0cd2b8cf53e4802065eb4112ddd68be3
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-03 09:19:18 +0100

    First part in

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -4,8 +4,33 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(_input: &str) {
+fn parse(input: &str) -> Vec<(i32, i32)> {
+    let joe: Vec<&str> = input.split("mul(").collect();
+    let new_joe = &joe[1..];
+    let mut results = Vec::new();
 
+    for part in new_joe.iter() {
+        if let Some(end_idx) = part.find(')') {
+            let content = &part[..end_idx];
+            if let Some((x, y)) = content.split_once(',') {
+                if let (Ok(x), Ok(y)) = (x.trim().parse::<i32>(), y.trim().parse::<i32>()) {
+                    results.push((x, y));
+                }
+            }
+        }
+    }
+    results
+}
+
+fn get_products(input: Vec<(i32, i32)>) -> i32 {
+    let product_sum: i32 = input.iter().map(|&(x, y)| x* y).sum();
+    product_sum
+}
+
+fn part1(input: &str) -> i32 {
+    let parsed_input = parse(input);
+    let product_sum = get_products(parsed_input);
+    product_sum
 }
 
 #[cfg(test)]
@@ -16,6 +41,6 @@ mod tests {
     fn it_works() {
         let input = include_str!("./example.txt");
         let result = part1(input);
-        assert_eq!(result, _ as i32);
+        assert_eq!(result, 161 as i32);
     }
 }
--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -1,11 +1,36 @@
 fn main() {
     let input = include_str!("./input.txt");
-    let output = part2(input);
+    let output = part1(input);
     dbg!(output);
 }
 
-fn part2(_input: &str) {
+fn parse(input: &str) -> Vec<(i32, i32)> {
+    let joe: Vec<&str> = input.split("mul(").collect();
+    let new_joe = &joe[1..];
+    let mut results = Vec::new();
 
+    for part in new_joe.iter() {
+        if let Some(end_idx) = part.find(')') {
+            let content = &part[..end_idx];
+            if let Some((x, y)) = content.split_once(',') {
+                if let (Ok(x), Ok(y)) = (x.trim().parse::<i32>(), y.trim().parse::<i32>()) {
+                    results.push((x, y));
+                }
+            }
+        }
+    }
+    results
+}
+
+fn get_products(input: Vec<(i32, i32)>) -> i32 {
+    let product_sum: i32 = input.iter().map(|&(x, y)| x* y).sum();
+    product_sum
+}
+
+fn part1(input: &str) -> i32 {
+    let parsed_input = parse(input);
+    let product_sum = get_products(parsed_input);
+    product_sum
 }
 
 #[cfg(test)]
@@ -15,7 +40,7 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("./example.txt");
-        let result = part2(input);
-        assert_eq!(result, _ as i32);
+        let result = part1(input);
+        assert_eq!(result, 48 as i32);
     }
 }
