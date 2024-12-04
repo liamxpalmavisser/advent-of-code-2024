f02c9a5786c866fcb1a9596e941d0b73e8271894
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-04 09:23:49 +0100

    Part2 day4 baby

--- day-04/src/bin/part2.rs
+++ day-04/src/bin/part2.rs
@@ -1,6 +1,6 @@
 fn main() {
     let input = include_str!("./input.txt");
-    let output = part1(input);
+    let output = part2(input);
     dbg!(output);
 }
 
@@ -8,18 +8,27 @@ fn get_coord_value(grid: &Vec<Vec<char>>, coord: (i32, i32)) -> Option<char> {
     let (y, x) = coord;
     if let Some(column) = grid.get(y as usize) {
         if let Some(value) = column.get(x as usize) {
-            return Some(*value); // Dereference to return char
+            return Some(*value);
         }
     }
     None
 }
 
-fn is_xmas(grid: &Vec<Vec<char>>, current: (i32, i32), dir: (i32, i32), depth: i32) -> bool {
-    let next = (current.0 + dir.0, current.1 + dir.1);
-    match (get_coord_value(grid, next), depth) {
-        (Some('M'), 0) => is_xmas(grid, next, dir, depth + 1),
-        (Some('A'), 1) => is_xmas(grid, next, dir, depth + 1),
-        (Some('S'), 2) => true,
+fn is_xmas(grid: &Vec<Vec<char>>, current: (i32, i32)) -> bool {
+    println!("{:?}", current);
+    match (
+        get_coord_value(grid, (current.0 - 1, current.1 - 1)),
+        get_coord_value(grid, (current.0 + 1, current.1 + 1)),
+    ) {
+        (Some('M'), Some('S')) | (Some('S'), Some('M')) => {
+            match (
+                get_coord_value(grid, (current.0 - 1, current.1 + 1)),
+                get_coord_value(grid, (current.0 + 1, current.1 - 1)),
+            ) {
+                (Some('M'), Some('S')) | (Some('S'), Some('M')) => true,
+                _ => false,
+            }
+        }
         _ => false,
     }
 }
@@ -28,30 +37,18 @@ fn get_grid(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn part1(input: &str) -> u32 {
+fn part2(input: &str) -> u32 {
     let grid = get_grid(input);
-    let directions = [
-        (0, 1),
-        (1, 0),
-        (0, -1),
-        (-1, 0),
-        (1, 1),
-        (-1, -1),
-        (-1, 1),
-        (1, -1),
-    ];
     let mut count = 0;
 
     for (y, row) in grid.iter().enumerate() {
         for (x, &ch) in row.iter().enumerate() {
-            if ch == 'X' {
+            if ch == 'A' {
                 let coord = (y as i32, x as i32);
 
-                for &dir in &directions {
-                    if is_xmas(&grid, coord, dir, 0) {
-                        println!("{:?}", coord);
-                        count += 1;
-                    }
+                if is_xmas(&grid, coord) {
+                    println!("{:?}", coord);
+                    count += 1;
                 }
             }
         }
@@ -67,7 +64,7 @@ mod tests {
     #[test]
     fn it_works() {
         let input = include_str!("./example.txt");
-        let result = part1(input);
+        let result = part2(input);
         assert_eq!(result, 18 as u32);
     }
 }
