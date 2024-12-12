fb79334c33cbee5abb2ed9c5773957d401ca6855
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-12 09:50:44 +0100

    Part2 difficult pffoe

--- day-12/src/bin/part1.rs
+++ day-12/src/bin/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 use utils::*;
 
 fn main() {
@@ -6,8 +8,60 @@ fn main() {
     dbg!(output);
 }
 
+fn parse(input: &str) -> Grid<u8> {
+    let grid = Grid::parse(input);
+    grid
+}
+
+fn flood_fill(grid: &Grid<u8>, point: Point, visited: &mut HashSet<Point>) -> (i32, i32) {
+    let mut queue: VecDeque<Point> = VecDeque::new();
+    let mut points_visited = 0;
+
+    let mut perimeter = 0;
+
+    queue.push_front(point);
+    visited.insert(point);
+
+    while let Some(current_point) = queue.pop_back() {
+        points_visited += 1;
+
+        for neighbor in current_point.neighbors() {
+            if grid.contains(neighbor) {
+                if grid[current_point] == grid[neighbor] && !visited.contains(&neighbor) {
+                    queue.push_front(neighbor);
+                    visited.insert(neighbor);
+                } else if grid[current_point] != grid[neighbor] {
+                    perimeter += 1;
+                }
+            } else {
+                perimeter += 1;
+            }
+        }
+    }
+
+    return (points_visited, perimeter);
+}
+
+fn calculate_cost(grid: &Grid<u8>) -> i32 {
+    let mut visited = HashSet::new();
+    let mut total_cost = 0;
+
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let point = Point::new(x, y);
+            if !visited.contains(&point) {
+                let (area, perimeter) = flood_fill(grid, point, &mut visited);
+                total_cost += area * perimeter
+            }
+        }
+    }
+
+    total_cost
+}
+
 fn part1(input: &str) -> i32 {
-    input.len() as i32
+    let grid = parse(input);
+    calculate_cost(&grid)
 }
 
 #[cfg(test)]
@@ -18,6 +72,6 @@ mod tests {
     fn it_works() {
         let input = include_str!("./example.txt");
         let result = part1(input);
-        assert_eq!(result, 21 as i32);
+        assert_eq!(result, 1930 as i32);
     }
 }
