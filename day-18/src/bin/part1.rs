7e5193014cdea0d960dbacd0022156aa0f5c0742
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-18 11:10:18 +0100

    Part1 day 18

--- day-18/src/bin/part1.rs
+++ day-18/src/bin/part1.rs
@@ -1,11 +1,61 @@
+use std::collections::{HashSet, VecDeque};
+
+use utils::*;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part1(input);
     dbg!(output);
 }
 
+fn parse(input: &str, n: usize) -> Grid<u8> {
+    let mut grid = Grid::new(71, 71, b'.');
+
+    let special_points: Vec<Point> = input
+        .lines().take(n)
+        .filter_map(|line| {
+            let mut parts = line.split(',').map(|p| p.trim().parse::<i32>());
+            if let (Some(Ok(x)), Some(Ok(y))) = (parts.next(), parts.next()) {
+                Some(Point::new(x, y))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    for &point in &special_points {
+        grid[point] = b'#';
+    }
+
+    grid
+}
+
+fn bfs(grid: Grid<u8>) -> i32 {
+    let mut queue = VecDeque::new();
+    let start = Point::new(0, 0);
+    let mut visited = HashSet::new();
+
+    queue.push_back((start, 0));
+
+    while let Some((current, depth)) = queue.pop_front() {
+        if current == Point::new(70, 70) {
+            return depth;
+        }
+
+        for neighbor in current.neighbors() {
+            if grid.contains(neighbor) && grid[neighbor] != b'#' && !visited.contains(&neighbor) {
+                queue.push_back((neighbor, depth + 1));
+                visited.insert(neighbor);
+            }
+        }
+    }
+    unreachable!()
+}
+
 fn part1(input: &str) -> i32 {
-    input.len() as i32
+    let grid = parse(input, 1024);
+    let depth = bfs(grid);
+    depth
 }
 
 #[cfg(test)]
@@ -16,6 +66,6 @@ mod tests {
     fn it_works() {
         let input = include_str!("./example.txt");
         let result = part1(input);
-        assert_eq!(result, 21 as i32);
+        assert_eq!(result, 22 as i32);
     }
 }
