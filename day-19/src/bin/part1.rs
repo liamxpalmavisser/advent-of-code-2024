ee45f45e8d3472dcac4ec8643b222ccc9ff19de3
liamxpalmavisser <liam.palmavisser@gmail.com>
2024-12-19 15:02:53 +0100

    Refactor dp solution

--- day-19/src/bin/part1.rs
+++ day-19/src/bin/part1.rs
@@ -5,7 +5,47 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-    input.len() as i32
+    let (available_towels, designs) = parse(input);
+
+    let mut possible_count = 0;
+
+    for design in designs {
+        dp(design, &available_towels, &mut possible_count);
+    }
+
+    return possible_count;
+}
+
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let available_towels = input.lines().next().expect("First line should be there");
+
+    let towels = available_towels
+        .split(",")
+        .map(|towel| towel.trim())
+        .collect();
+
+    let designs = input.lines().skip(2).collect();
+
+    return (towels, designs);
+}
+
+fn dp(design: &str, available_towels: &Vec<&str>, possible_count: &mut i32) {
+    let mut is_possible = vec![false; design.len() + 1];
+    is_possible[0] = true;
+
+    for i in 0..design.len() {
+        if is_possible[i] {
+            for towel in available_towels {
+                if design[i..].starts_with(towel) {
+                    is_possible[i + towel.len()] = true;
+                }
+            }
+        }
+    }
+
+    if is_possible[design.len()] {
+        *possible_count += 1;
+    }
 }
 
 #[cfg(test)]
@@ -16,6 +56,6 @@ mod tests {
     fn it_works() {
         let input = include_str!("./example.txt");
         let result = part1(input);
-        assert_eq!(result, 21 as i32);
+        assert_eq!(result, 6 as i32);
     }
 }
--- day-19/src/bin/part2.rs
+++ day-19/src/bin/part2.rs
@@ -4,8 +4,48 @@ fn main() {
     dbg!(output);
 }
 
-fn part2(input: &str) -> i32 {
-    input.len() as i32
+fn part2(input: &str) -> i64 {
+    let (available_towels, designs) = parse(input);
+
+    let mut possible_count = 0;
+
+    for design in designs {
+        let result = dp(design, &available_towels);
+
+        possible_count += result;
+    }
+
+    return possible_count;
+}
+
+fn dp(design: &str, available_towels: &Vec<&str>) -> i64 {
+    let mut n_options = vec![0; design.len() + 1];
+    n_options[0] = 1;
+
+    for i in 0..design.len() {
+        if n_options[i] > 0 {
+            for towel in available_towels {
+                if design[i..].starts_with(towel) {
+                    n_options[i + towel.len()] += n_options[i];
+                }
+            }
+        }
+    }
+
+    return n_options[design.len()];
+}
+
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let available_towels = input.lines().next().expect("First line should be there");
+
+    let towels = available_towels
+        .split(",")
+        .map(|towel| towel.trim())
+        .collect();
+
+    let designs = input.lines().skip(2).collect();
+
+    return (towels, designs);
 }
 
 #[cfg(test)]
@@ -16,6 +56,6 @@ mod tests {
     fn it_works() {
         let input = include_str!("./example.txt");
         let result = part2(input);
-        assert_eq!(result, 21 as i32);
+        assert_eq!(result, 16 as i64);
     }
 }
